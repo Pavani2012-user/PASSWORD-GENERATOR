357ad8c77247c8b98fe961089b7040f19a38b6b0
Pavani2012-user <pavanibattu006@gmail.com>
2025-01-28 16:19:38 +0530

    Add files via upload

---
+++ Passwordgenerator.java
@@ -0,0 +1,20 @@
+
+import java.security.SecureRandom;
+
+public class Passwordgenerator {
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_-+=<>?";
+    private static final int PASSWORD_LENGTH = 12;
+
+    public static void main(String[] args) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder password = new StringBuilder(PASSWORD_LENGTH);
+
+        for (int i = 0; i < PASSWORD_LENGTH; i++) {
+            int index = random.nextInt(CHARACTERS.length());
+            password.append(CHARACTERS.charAt(index));
+        }
+
+        System.out.println("Generated Password: " + password.toString());
+    }
+}
+
